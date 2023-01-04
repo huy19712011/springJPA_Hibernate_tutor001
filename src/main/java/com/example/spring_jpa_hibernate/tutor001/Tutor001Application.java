c919936eaf583e66abe2e09429b3251103f63625
huynq <huy19712011@gmail.com>
2023-01-04 22:48:57 +0700

    Step 05, 06: findAll() using Spring JDBC and execute method using CommandLineRunner

--- src/main/java/com/example/spring_jpa_hibernate/tutor001/Tutor001Application.java
+++ src/main/java/com/example/spring_jpa_hibernate/tutor001/Tutor001Application.java
@@ -1,14 +1,28 @@
 package com.example.spring_jpa_hibernate.tutor001;
 
+import com.example.spring_jpa_hibernate.tutor001.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tutor001Application {
+public class Tutor001Application implements CommandLineRunner {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tutor001Application.class, args);
 		System.out.println("Setting up ... Ok!!!");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findAll());
+	}
 }
---
+++ src/main/java/com/example/spring_jpa_hibernate/tutor001/jdbc/PersonJdbcDao.java
@@ -0,0 +1,21 @@
+package com.example.spring_jpa_hibernate.tutor001.jdbc;
+
+import com.example.spring_jpa_hibernate.tutor001.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
