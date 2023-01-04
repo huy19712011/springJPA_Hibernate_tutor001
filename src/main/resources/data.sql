36dfda503bbcd70d09038bb92088404e0e06bf30
huynq <huy19712011@gmail.com>
2023-01-04 22:05:55 +0700

    Step 04: Populate data into table (H2)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,12 @@ create table person(
     birth_date timestamp,
     primary key (id)
 );
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES
+    (100001, 'ASD', 'MY DINH', CURDATE()),
+    (100002, 'FGH', 'MY DINH', CURDATE()),
+    (100003, 'JKL', 'MY DINH', CURDATE()),
+    (100004, 'JKL', 'MY DINH', CURRENT_TIMESTAMP()),
+--    (100009, 'JKL', 'MY DINH', SYSDATE()), -- OK on MySQL, but not H2
+    (100005, 'JKL', 'MY DINH', CURRENT_DATE())
