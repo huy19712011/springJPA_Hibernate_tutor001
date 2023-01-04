5b974e4342dab28feeabc9375fd56c8079e07b6c
huynq <huy19712011@gmail.com>
2023-01-04 16:31:48 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_jpa_hibernate/tutor001/Tutor001Application.java
@@ -0,0 +1,14 @@
+package com.example.spring_jpa_hibernate.tutor001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutor001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutor001Application.class, args);
+		System.out.println("Setting up ... Ok!!!");
+	}
+
+}
---
+++ src/test/java/com/example/spring_jpa_hibernate/tutor001/Tutor001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_hibernate.tutor001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutor001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
